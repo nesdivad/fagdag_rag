e5f5cc98df757c731e7cd73b93c04f0775ef678a
Kristoffer Davidsen <88896307+nesdivad@users.noreply.github.com>
2025-02-05 17:21:25 +0100

    storage service

---
+++ fagdag.utils/StorageService.cs
@@ -0,0 +1,43 @@
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+
+namespace Fagdag.Utils;
+
+public interface IStorageService
+{
+    IAsyncEnumerable<BlobItem> DownloadBlobsAsync();
+}
+
+public class StorageService : IStorageService
+{
+    private const string BlobContainerName = "fagdag";
+    private BlobServiceClient _client { get; init; }
+
+    public StorageService(string connectionString)
+    {
+        ArgumentNullException.ThrowIfNullOrEmpty(connectionString);
+
+        _client = new(connectionString);
+    }
+
+    public async IAsyncEnumerable<BlobItem> DownloadBlobsAsync()
+    {
+        var blobClient = _client.GetBlobContainerClient(BlobContainerName);
+        if (blobClient is null)
+            blobClient = await CreateBlobContainerAsync();
+
+        await foreach (var blob in blobClient.GetBlobsAsync())
+        {
+            if (blob is null)
+                continue;
+
+            yield return blob;
+        }
+    }
+
+    private async Task<BlobContainerClient> CreateBlobContainerAsync()
+    {
+        var res = await _client.CreateBlobContainerAsync(BlobContainerName);
+        return res.Value;
+    }
+}
